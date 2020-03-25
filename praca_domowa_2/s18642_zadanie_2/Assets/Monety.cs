b7ca6f63cab4f117a7c2a4ae74c0abc202050eae
s18642 <61684596+s18642@users.noreply.github.com>
2020-03-25 20:19:54 +0100

    Zadanie_2_v_1.0
    Scena pustynna; Bohater się porusza, zbiera monety etc, zabija przeciwnika po prostu go dotykając

---
+++ praca_domowa_2/s18642_zadanie_2/Assets/Monety.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monety : MonoBehaviour
+{
+    public GameObject monety;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(monety);
+    }
+
+}
