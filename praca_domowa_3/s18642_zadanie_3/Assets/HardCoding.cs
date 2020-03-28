a4c2dcac84c335c959fd8e7d55c930c74f4876f0
s18642 <61684596+s18642@users.noreply.github.com>
2020-03-28 13:02:24 +0100

    Zadanie_3_v1.0
    Gotowe

--- praca_domowa_3/s18642_zadanie_3/Assets/HardCoding.cs
+++ praca_domowa_3/s18642_zadanie_3/Assets/HardCoding.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class HardCoding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void Start()
     {
-        
+        int liczba1 = 10;
+        float liczba2 = 20.0f;
+
+        bool isCodingHard = true;
+        bool isCodingLame = false;
+
+        string napis1 = "Programuję w C#";
+        string napis2 = "EPG";
+
+        Debug.Log(liczba1 + ", " + liczba2 + ", " + isCodingHard + ", " + isCodingLame + ", " + napis1 + ", " + napis2);
+
+        PorownanieLiczb(liczba1, liczba2);
+        PorownanieStringow(napis1, napis2);
+        Koniunkcja(isCodingLame, isCodingHard);
+        Alternatywa(isCodingLame, isCodingHard);
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PorownanieLiczb(int liczba1, float liczba2)
     {
-        
+        if (liczba1 == liczba2)
+        {
+            Debug.Log("Tak");
+        }
+        else if ((liczba1 != liczba2) || (liczba1 > liczba2) || (liczba1 < liczba2) || (liczba1 >= liczba2) || (liczba1 <= liczba2))
+        {
+            Debug.Log("Nie");
+        }
     }
+
+    public void PorownanieStringow(string napis1, string napis2)
+    {
+          if (!napis1.Equals(napis2))
+        {
+            Debug.Log("różne");
+        }
+    }
+
+    public void Koniunkcja(bool isCodingLame, bool isCodingHard)
+    {
+        if ((isCodingLame == true) && (isCodingHard == true))
+        {
+            Debug.Log("true");
+        }
+        else
+        {
+            Debug.Log("false");
+        }
+    }
+
+    public void Alternatywa(bool isCodingLame, bool isCodingHard)
+    {
+        if ((isCodingLame == false) || (isCodingHard == false))
+        {
+            if ((isCodingHard == true) || (isCodingLame == true))
+            {
+                Debug.Log("true");
+            }
+            else
+            {
+                Debug.Log("false");
+            }
+        }
+    }
+
+    
 }
